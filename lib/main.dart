fcad039a188c985411929b8f36d34a6113aa16da
Rykrt25 <rifkyalmukmin@gmail.com>
2023-01-01 22:39:46 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Color color1 = Colors.red;
+  Color color2 = Colors.amber;
+  late Color targetColor;
+  bool isAccepted = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Latihan dragable"),
+        ),
+        body: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Draggable<Color>(
+                    data: color1,
+                    childWhenDragging: const SizedBox(
+                      width: 50,
+                      height: 50,
+                      child: Material(
+                        color: Colors.grey,
+                        shape: StadiumBorder(),
+                        elevation: 3,
+                      ),
+                    ),
+                    feedback: SizedBox(
+                      width: 50,
+                      height: 50,
+                      child: Material(
+                        color: color1.withOpacity(0.7),
+                        shape: const StadiumBorder(),
+                        elevation: 3,
+                      ),
+                    ),
+                    child: SizedBox(
+                      width: 50,
+                      height: 50,
+                      child: Material(
+                        color: color1,
+                        shape: const StadiumBorder(),
+                        elevation: 3,
+                      ),
+                    ),
+                  ),
+                  Draggable<Color>(
+                    data: color2,
+                    childWhenDragging: const SizedBox(
+                      width: 50,
+                      height: 50,
+                      child: Material(
+                        color: Colors.grey,
+                        shape: StadiumBorder(),
+                        elevation: 3,
+                      ),
+                    ),
+                    feedback: SizedBox(
+                      width: 50,
+                      height: 50,
+                      child: Material(
+                        color: color2.withOpacity(0.7),
+                        shape: const StadiumBorder(),
+                        elevation: 3,
+                      ),
+                    ),
+                    child: SizedBox(
+                      width: 50,
+                      height: 50,
+                      child: Material(
+                        color: color2,
+                        shape: const StadiumBorder(),
+                        elevation: 3,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              DragTarget<Color>(
+                onWillAccept: (value) => true,
+                onAccept: (value) {
+                  isAccepted = true;
+                  targetColor = value;
+                },
+                builder: (context, candidateData, rejectedData) {
+                  return (isAccepted)
+                      ? SizedBox(
+                          width: 100,
+                          height: 100,
+                          child: Material(
+                            color: targetColor,
+                            shape: const StadiumBorder(),
+                          ),
+                        )
+                      : const SizedBox(
+                          width: 100,
+                          height: 100,
+                          child: Material(
+                            color: Colors.grey,
+                            shape: StadiumBorder(),
+                          ),
+                        );
+           },
+          )
+        ]),
+      ),
+    );
+  }
+}
